f7b6dd34a7372a03c865fbe7285b0b5f0f55b37b
claudio <claudiocardoso9736@gmail.com>
2025-11-24 17:59:23 -0300

    23-11

---
+++ js/claudio.js
@@ -0,0 +1,11 @@
+
+
+const typed = new Typed (".auto-type", {
+    strings: ["SEGURANÇA ", "começa por voçe-e salva todos ao redor"],
+    typeSpeed: 80, 
+    backSpeed: 60,
+    backDelay: 1200,
+    loop: true ,
+    showCursor: false
+    
+} );
